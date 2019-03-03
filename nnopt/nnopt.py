b89f3aed4492ea438163d4a5c7ef4020771ff493
jonas@valfridsson.net <jonas@valfridsson.net>
2019-03-03 17:03:06 +0100

    Derp. Fix missing constant post rename

--- nnopt/nnopt.py
+++ nnopt/nnopt.py
@@ -1,25 +1,25 @@
 import tensorflow as tf
 import tensorflow_probability as tfp
+from sklearn.covariance import EmpiricalCovariance
 import numpy as np
 import time
 import random
 
 
 class Optimizer():
-    SCOPE = "nnopt"
 
     def __init__(
             self,
             F: "F: R^N -> R^1",
             N: int,
             surrogate_hidden_layer: int = 60,
-            kernel_seperate: int = 60,
             kernel_common: int = 60,
             Rn:
             "[Range_0, Range_1...Range_n] N = Range_n = (Min_n, Max_n)" = None,
             convergence_limit: float = 1e-5,
             codomain_norm:
-            "F: R^1 x R^1 -> R^1" = lambda x, y: np.sum(np.abs(x - y)),
+            "F: R^1 x R^1 -> R^1" = lambda x, y: np.mean(np.abs(x - y)),
+            scope: str = "nnopt",
             seed=None):
 
         self.N = N
@@ -30,9 +30,11 @@ class Optimizer():
 
         self.minis = None
         self.maxis = None
-        if Rn and len(Rn) != N:
+        if Rn and len(Rn) != N and isinstance(Rn, list):
             raise Exception("Ranges must match domain")
-        if Rn:
+        elif isinstance(Rn, tuple):
+            self.minis, self.maxis = Rn[0], Rn[1]
+        elif Rn:
             self.minis, self.maxis = zip(*Rn)
 
         self.F = F
@@ -45,16 +47,23 @@ class Optimizer():
         self.k2_features = []
         self.kernel_labels = []
 
-        self.kernel_lr = tf.Variable(1e-3)
-        self.surrogate_lr = tf.Variable(1e-3)
+        with tf.variable_scope(scope, reuse=tf.AUTO_REUSE):
+            self.kernel_lr = tf.Variable(1e-3)
+            self.surrogate_lr = tf.Variable(1e-3)
 
-        with tf.variable_scope(Optimizer.SCOPE, reuse=tf.AUTO_REUSE):
             self.N_gen = None
-            if Rn:
+            if Rn and isinstance(Rn, list):
                 self.N_gen = tf.concat(
                     [
                         tf.random_uniform([1], mini, maxi, seed=seed)
                         for mini, maxi in Rn
+                    ],
+                    axis=0)
+            elif Rn and isinstance(Rn, tuple):
+                self.N_gen = tf.concat(
+                    [
+                        tf.random_uniform([1], Rn[0], Rn[1], seed=seed)
+                        for _ in range(N)
                     ],
                     axis=0)
             else:
@@ -94,13 +103,19 @@ class Optimizer():
 
             surrogate_variables = tf.get_collection(
                 tf.GraphKeys.TRAINABLE_VARIABLES,
-                scope="{}/surrogate".format(Optimizer.SCOPE))
+                scope="{}/surrogate".format(scope))
 
             self.surrogate_error = tf.losses.mean_squared_error(
-                self.OBS, self.OUT)
+                self.OBS, self.OUT) + 1e-6 * tf.reduce_mean(
+                    [tf.reduce_mean(var) for var in surrogate_variables])
 
             surrogate_grads = tf.gradients(self.surrogate_error,
                                            surrogate_variables)
+
+            surrogate_grads = [
+                tf.clip_by_value(grad, -10, 10) for grad in surrogate_grads
+            ]
+
             self.surrogate_opt = tf.train.AdamOptimizer(
                 learning_rate=self.surrogate_lr).apply_gradients(
                     zip(surrogate_grads, surrogate_variables))
@@ -115,74 +130,41 @@ class Optimizer():
 
             self.K_labels = tf.placeholder(tf.float32, shape=[None])
 
-            with tf.variable_scope("kernel", reuse=tf.AUTO_REUSE):
-                hidden = tf.concat([self.K1, self.K2], 1)
-                hidden = tf.layers.dense(
-                    hidden,
-                    kernel_common,
-                    activation=tf.math.sigmoid,
-                    use_bias=True)
-                hidden = tf.layers.dense(
-                    hidden,
-                    kernel_common,
-                    activation=tf.math.sigmoid,
-                    use_bias=True)
-                hidden = tf.layers.dense(
-                    hidden, kernel_common, activation=None, use_bias=True)
-                self.K = tf.squeeze(
-                    tf.layers.dense(hidden, 1, activation=None, use_bias=True))
-
-            kernel_variables = tf.get_collection(
-                tf.GraphKeys.TRAINABLE_VARIABLES,
-                scope="{}/kernel".format(Optimizer.SCOPE))
-
-            self.kernel_error = tf.losses.mean_squared_error(
-                self.K_labels, self.K)
-            kernel_grads = tf.gradients(self.kernel_error, kernel_variables)
-            self.kernel_opt = tf.train.AdamOptimizer(
-                learning_rate=self.kernel_lr).apply_gradients(
-                    zip(kernel_grads, kernel_variables))
-
         self.local_init = tf.initializers.variables(
             tf.get_collection(
-                tf.GraphKeys.GLOBAL_VARIABLES, scope=Optimizer.SCOPE))
+                tf.GraphKeys.GLOBAL_VARIABLES, scope=scope))
 
         self.S = tf.Session()
         self.S.run(self.local_init)
 
-    def sample(self, N=[], verbose=True) -> None:
+    def sample(self, N=[], verbose=2) -> None:
         if len(N) == 0:
             N = self.S.run(self.N_gen)
 
         M = self.F(*N)
-        if verbose:
-            print("target", M)
+        if verbose == 2:
+            print("I", self.samples, "target", M)
+
+        if verbose == 1 and self.M_samples and M > max(self.M_samples):
+            print("I", self.samples, "target", M)
 
         self.N_samples.append(N)
         self.M_samples.append(M)
 
         self.samples += 1
 
-        for i in range(self.samples):
-            self.k1_features.append(self.N_samples[-1])
-            self.k2_features.append(self.N_samples[i])
-            self.kernel_labels.append(
-                self.codomain_norm(self.M_samples[-1], self.M_samples[i]))
-
-            self.k1_features.append(self.N_samples[i])
-            self.k2_features.append(self.N_samples[-1])
-            self.kernel_labels.append(
-                self.codomain_norm(self.M_samples[i], self.M_samples[-1]))
-
     def predict(self, N) -> (np.ndarray, np.ndarray):
         return self.S.run(self.OUT, feed_dict={self.IN: N})
 
-    def kernel(self, k1, k2) -> [float]:
-        return self.S.run(self.K, feed_dict={self.K1: k1, self.K2: k2})
+    def fit(self, epochs=10) -> float:
 
-    def fit(self, epochs=10, lr=1e-3) -> float:
-        self.S.run((self.kernel_lr.assign(lr), self.surrogate_lr.assign(lr)))
+        lr = 1e-3
+        self.S.run(self.surrogate_lr.assign(lr))
+        adapted = True
+        adaptivity = 100
+        p = np.zeros(adaptivity)
 
+        mod_2 = 0
         for i in range(epochs):
             _, err = self.S.run(
                 (self.surrogate_opt, self.surrogate_error),
@@ -191,20 +173,29 @@ class Optimizer():
                     self.OBS: self.M_samples
                 })
 
+            # print("I surr", i, "err", err)
+
+            diff = np.mean(p) / err
+            if diff > 0.9 and i % adaptivity == adaptivity - 1:
+                if not adapted:
+                    break
+                lr /= 2
+                self.S.run(self.surrogate_lr.assign(lr))
+
+                adapted = False
+
+            if diff < 0.9:
+                adapted = True
+
             if err < self.convergence_limit:
                 break
+            """ Avoid oscillation. """
+            if err == mod_2:
+                lr /= 2
+                self.S.run(self.surrogate_lr.assign(lr))
 
-        for i in range(min(max(epochs // 5, 400), 500)):
-            _, err = self.S.run(
-                (self.kernel_opt, self.kernel_error),
-                feed_dict={
-                    self.K1: self.k1_features,
-                    self.K2: self.k2_features,
-                    self.K_labels: self.kernel_labels
-                })
-
-            if err < self.convergence_limit:
-                break
+            if i % 2:
+                mod_2 = err
 
     def calc_suggestions(self):
         """ Used to normalize """
@@ -212,7 +203,13 @@ class Optimizer():
         suggs = []
         preds = []
         ents = []
+        """ Quick fix till i come up with an idea to approximate the manifold.."""
+        inv_cov = np.linalg.inv(
+            EmpiricalCovariance(
+                assume_centered=True).fit(self.N_samples, self.M_samples)
+            .covariance_ + np.eye(self.N) * 1e-6)
 
+        # Exploitation
         for p in range(self.samples):
             gradients, inv_hess = self.S.run(
                 (self.IN_grads, self.IN_hessian),
@@ -228,9 +225,12 @@ class Optimizer():
 
             pred = self.predict([suggestion])
 
-            ent = 1e6
-            for k2 in self.N_samples:
-                ent = min(self.kernel([suggestion], [k2]), ent)
+            ent = 1000000
+            for point in self.N_samples:
+                ent = min(
+                    ent,
+                    np.sqrt((point - suggestion).reshape(1, -1) @ inv_cov @ (
+                        point - suggestion).reshape(-1, 1)))
 
             # print("Start", self.N_samples[p], "Score", self.M_samples[p])
             # print("Recommendation", suggestion, "Pred", pred, "Ent", ent)
@@ -243,17 +243,38 @@ class Optimizer():
             preds.append(pred)
             ents.append(ent)
 
-        preds = np.array(preds)
-        ents = np.array(ents)
+        # Exploration..
+        for p in range(10):
+            suggestion = self.S.run(self.N_gen).reshape(-1)
+            pred = self.predict([suggestion])
+
+            ent = 1000000
+            for point in self.N_samples:
+                ent = min(
+                    ent,
+                    np.sqrt((point - suggestion).reshape(1, -1) @ inv_cov @ (
+                        point - suggestion).reshape(-1, 1)))
+
+            # print("Recommendation", suggestion, "Pred", pred, "Ent", ent)
+            # print()
+
+            suggs.append(suggestion)
+            preds.append(pred)
+            ents.append(ent)
+
+        suggs = np.array(suggs)
+        preds = np.array(preds).reshape(-1)
+        ents = np.array(ents).reshape(-1)
 
         mean_pred = np.mean(preds)
         stdev_pred = np.std(preds)
+
         mean_ent = np.mean(ents)
         stdev_ent = np.std(ents)
 
         # print("MEANENT VARENT", np.mean(ents), np.var(ents))
-        preds = (preds - mean_pred) / (stdev_pred + 1e-6)
-        ents = (ents - mean_ent) / (stdev_ent + 1e-6)
+        preds = (preds - mean_pred) / stdev_pred
+        ents = (ents - mean_ent) / stdev_ent
 
         return suggs, preds, ents
 
@@ -273,21 +294,18 @@ class Optimizer():
             optimization=10,
             fitting=1000,
             exploration=1.0,
-            lr=1e-3,
-            verbose=True):
+            verbose=2):
         for i in range(random):
             self.sample(verbose=verbose)
 
-        if verbose:
+        if verbose == 2 or verbose == 1:
             print("Best random", self.N_samples[np.argmax(self.M_samples)],
                   max(self.M_samples))
 
-        self.fit(fitting, lr=lr)
-
         for i in range(optimization):
+            self.fit(fitting)
             sugg = self.optimize(exploration=exploration)
             self.sample(sugg, verbose=verbose)
-            self.fit(fitting, lr=lr)
 
         return self.N_samples[np.argmax(self.M_samples)], max(self.M_samples)
 
